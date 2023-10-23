ef56ad86c727b60d15942c83ca383ebbbb1b02e4
MasterSlyer10 <masterslyer10@gmail.com>
2023-10-23 20:53:53 +0800

    Integrating menu modal to different pages

--- js/hamburger-menu-modal.js
+++ js/hamburger-menu-modal.js
@@ -8,12 +8,12 @@ hamburgerButton.addEventListener('click', () => {
   menuDropdownContainer.style.backgroundColor = "rgba(113, 113, 113, 0.3)";
   menuDropdownContainer.style.alignItems = "flex-start";
   menuDropdownContainer.style.justifyContent = "flex-end";
-  menuDropdownContainer.setAttribute("clossable", "true")
-});
-
-// Hide the menu dropdown container when the user clicks outside of it
-window.addEventListener('click', (event) => {
-  if (event.target !== hamburgerButton && !menuDropdownContainer.contains(event.target)) {
-    menuDropdownContainer.style.display = 'none';
-  }
+  menuDropdownContainer.setAttribute("closable", "");
+  
+  const onClick = (event) => {
+    if (event.target === menuDropdownContainer && menuDropdownContainer.hasAttribute("closable")) {
+      menuDropdownContainer.style.display = 'none';
+    }
+  };
+  menuDropdownContainer.addEventListener('click', onClick);
 });
