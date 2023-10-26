9268367813912a84b4b9ca98d852915e46627e2f
MasterSlyer10 <masterslyer10@gmail.com>
2023-10-26 18:22:06 +0800

    Fix linking issues

--- js/hamburger-menu-modal.js
+++ js/hamburger-menu-modal.js
@@ -28,5 +28,5 @@ simulationButton.addEventListener('click', () => {
 
 // Event handler for "Lessons"
 lesseonsButton.addEventListener('click', () => {
-  window.location.href = "lessons.html"; // should change to lessons
+  window.location.href = "lesson-picker.html"; // should change to lessons
 });
