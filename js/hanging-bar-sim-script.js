e67579286282fcb7fa8b06cccdc90ac1e615402f
MasterSlyer10 <masterslyer10@gmail.com>
2023-11-06 03:56:41 +0800

    Implemented tabs
    add content to "snap" tab coming up

--- js/hanging-bar-sim-script.js
+++ js/hanging-bar-sim-script.js
@@ -795,3 +795,22 @@ const exitButton = document.getElementById('exit-button');
 exitButton.addEventListener('click', () => {
     window.location.href = "../index.html";
 });
+
+
+
+// For Switching the materialSelect tabs
+function openTabs(evt, tabName) {
+    let i, tabContent, tabLinks;
+
+    tabContent = document.getElementsByClassName("tab");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].classList.remove("active");
+
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+  }
